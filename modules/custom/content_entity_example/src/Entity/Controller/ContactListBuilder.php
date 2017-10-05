f2a224d6f6e50bd6606a89a7da7bbc90ab91db95
Huawei Chen <huawei.chen@xvt.com.au>
2017-10-05 15:09:50 +1000

    Day 11: plugin example

--- modules/custom/content_entity_example/src/Entity/Controller/ContactListBuilder.php
+++ modules/custom/content_entity_example/src/Entity/Controller/ContactListBuilder.php
@@ -6,4 +6,48 @@ use \Drupal\Core\Entity\EntityListBuilder;
 
 class ContactListBuilder extends EntityListBuilder {
 
+  /**
+   * @inheritdoc
+   *
+   */
+  public function render() {
+    $build['description'] = [
+      '#markup' => $this->t('Content Entity Example implements a Contacts model. These contacts are fieldable entities. You can manage the fields on the <a href="@adminlink">Contacts admin page</a>.', array(
+        '@adminlink' => \Drupal::urlGenerator()
+          ->generateFromRoute('content_entity_example.contact_settings'),
+      )),
+    ];
+
+    $build += parent::render();
+    return $build;
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * Building the header and content lines for the contact list.
+   *
+   * Calling the parent::buildHeader() adds a column for the possible actions
+   * and inserts the 'edit' and 'delete' links as defined for the entity type.
+   */
+  public function buildHeader() {
+    $header['id'] = $this->t('ContactID');
+    $header['name'] = $this->t('Name');
+    $header['first_name'] = $this->t('First Name');
+    $header['gender'] = $this->t('Gender');
+    return $header + parent::buildHeader();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildRow(EntityInterface $entity) {
+    /* @var $entity \Drupal\content_entity_example\Entity\Contact */
+    $row['id'] = $entity->id();
+    $row['name'] = $entity->link();
+    $row['first_name'] = $entity->first_name->value;
+    $row['gender'] = $entity->gender->value;
+    return $row + parent::buildRow($entity);
+  }
+
 }
